3250dcee15465f92677afdf29f787270275df15a
Jeevu <jeevanbanstola16@gmail.com>
2024-01-30 22:29:21 +0545

    handle pagination

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/3.7.9/flutter"
+export "FLUTTER_APPLICATION_PATH=/Applications/flutter_applications/table_pagination/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Applications/flutter_applications/table_pagination/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Applications/flutter_applications/table_pagination/example/.dart_tool/package_config.json"
